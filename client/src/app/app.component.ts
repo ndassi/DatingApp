2e787ff63d52661fe9239e8e57d3e2464b4f8cf6
Panda Hereos <pandaheroes.ph@gmail.com>
2024-02-21 10:14:29 -0500

    first base client and api app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,33 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { errorMonitor } from 'events';
-import { resourceUsage } from 'process';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit{
   title = 'client';
-
-  users:any;
-  /**
-   *
-   */
-  constructor(private http: HttpClient) {
-    
-    
-  }
+  users : any;
+ 
+  constructor(private http: HttpClient) { }
   ngOnInit(): void {
     this.http.get("https://localhost:7102/api/users").subscribe({
-        next: respond =>{ console.log(respond); this.users = respond;},
-        error : (error)=>{console.log(error)},
-        complete:()=>{console.log("Request has completed !!!")}
-
+      next : response => { this.users=response; console.log(response);},
+      error: error=> console.log(error),
+      complete: ()=>console.log("Request has completed")
+      
+      
     })
   }
 
